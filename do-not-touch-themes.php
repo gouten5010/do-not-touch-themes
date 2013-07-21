b2d6b955292b1bf9c982197e0bf09ea79e4b60d9
GOUTEN <gouten@5010works.com>
2013-07-22 01:05:32 +0900

    暫定でファイルアップ
    本来はテーマ選択とインストールをremove_cap()で無効化したかったが、プラグインを無効にしたさいにその権限をもとに戻す方法がわからなかっ
    たため、暫定で該当のページにアクセスした時にエラーメッセージが出るようにしています。

---
+++ do-not-touch-themes.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Don't Touch Themes
+Plugin URI: https://github.com/gouten5010/do-not-touch-themes
+Description: We do not want to make a theme chosen, and do not want to let edit of WordPress plug-in and WordPress theme.
+Version:0.0.1
+Author:GOUTEN
+Author URI: http://5010works.com/
+License: GPL v2
+License URI:
+*/
+
+function non_edit_theme_and_plugin() {
+	$request_url = "http://".$_SERVER[ "HTTP_HOST"] . $_SERVER["PHP_SELF"];
+	$theme_select_url = get_admin_url( 'themes.php', 'themes.php' );
+	$theme_isntall_url = get_admin_url( 'themes.php', 'theme-install.php' );
+
+	//テーマ・プラグインの編集をさせないようにする
+	if ( !defined ( 'DISALLOW_FILE_EDIT' ) ) {
+		define( 'DISALLOW_FILE_EDIT', true );
+	}
+
+	//themes.phpにアクセスした場合エラーメッセージを表示
+	switch ($request_url) {
+		case $theme_select_url:
+		case $theme_isntall_url:
+			wp_die( __( 'You do not have sufficient permissions to manage options for this site.' ) );
+	}
+}
+add_action('plugins_loaded', 'non_edit_theme_and_plugin');
+
+function remove_theme_select_menus () {
+	//テーマメニューを管理画面から除去
+	remove_submenu_page( 'themes.php', 'themes.php' );
+}
+add_action( 'admin_menu', 'remove_theme_select_menus' );
