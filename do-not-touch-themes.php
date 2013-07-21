bbd25750a14c96fe8fe80b1bc95debc985266e71
GOUTEN <gouten@5010works.com>
2013-07-22 03:18:50 +0900

    map_meta_capで個別権限削除の処理追加
    これを受けてページを照合してのエラーメッセージの表示を削除しました。

--- do-not-touch-themes.php
+++ do-not-touch-themes.php
@@ -11,20 +11,9 @@ License URI:
 */
 
 function non_edit_theme_and_plugin() {
-	$request_url = "http://".$_SERVER[ "HTTP_HOST"] . $_SERVER["PHP_SELF"];
-	$theme_select_url = get_admin_url( 'themes.php', 'themes.php' );
-	$theme_isntall_url = get_admin_url( 'themes.php', 'theme-install.php' );
-
 	//テーマ・プラグインの編集をさせないようにする
 	if ( !defined ( 'DISALLOW_FILE_EDIT' ) ) {
 		define( 'DISALLOW_FILE_EDIT', true );
-	}
-
-	//themes.phpにアクセスした場合エラーメッセージを表示
-	switch ($request_url) {
-		case $theme_select_url:
-		case $theme_isntall_url:
-			wp_die( __( 'You do not have sufficient permissions to manage options for this site.' ) );
 	}
 }
 add_action('plugins_loaded', 'non_edit_theme_and_plugin');
@@ -35,14 +24,14 @@ function remove_theme_select_menus () {
 }
 add_action( 'admin_menu', 'remove_theme_select_menus' );
 
-//function remove_role_options(){
-//	$roles = get_role('administrator');
-//	$caps = array(
-//		'switch_themes',
-//		'install_themes'
-//	);
-//	foreach ( $caps as $cap ){
-//		$roles->remove_cap( $cap );
-//	}
-//}
-//add_action( 'admin_init', 'remove_role_options' );
+function remove_role_options( $caps, $cap ) {
+	$capabilities = array(
+		'switch_themes',
+		'install_themes'
+	);
+	if (in_array($cap, $capabilities)) {
+		$caps[] = 'do_not_allow';
+	}
+	return $caps;
+}
+add_filter('map_meta_cap', 'remove_role_options', 10, 2);
