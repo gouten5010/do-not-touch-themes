53aed3b06883267ab2ede489565d5690581b3bf3
GOUTEN <gouten@5010works.com>
2013-07-22 03:27:52 +0900

    注意書きの追加・整形

--- do-not-touch-themes.php
+++ do-not-touch-themes.php
@@ -11,10 +11,16 @@ License URI:
 */
 
 function non_edit_theme_and_plugin() {
-	//テーマ・プラグインの編集をさせないようにする
+	/*
+	テーマ・プラグインの編集をさせないようにする
+	(DISALLOW_FILE_MODSを使用する場合は削除するコト)
+	*/
 	if ( !defined ( 'DISALLOW_FILE_EDIT' ) ) {
 		define( 'DISALLOW_FILE_EDIT', true );
 	}
+	/*
+	テーマ・プラグインの新規インストールとアップデート、編集をさせないようにする
+	*/
 	//if ( !defined ( 'DISALLOW_FILE_MODS' ) ) {
 	//	define('DISALLOW_FILE_MODS',true);
 	//}
@@ -22,7 +28,9 @@ function non_edit_theme_and_plugin() {
 add_action('plugins_loaded', 'non_edit_theme_and_plugin');
 
 function remove_theme_select_menus () {
-	//テーマメニューを管理画面から除去
+	/*
+	「テーマ」メニューを管理画面から除去
+	*/
 	remove_submenu_page( 'themes.php', 'themes.php' );
 }
 add_action( 'admin_menu', 'remove_theme_select_menus' );
@@ -30,7 +38,6 @@ add_action( 'admin_menu', 'remove_theme_select_menus' );
 function remove_role_options( $caps, $cap ) {
 	$capabilities = array(
 		'switch_themes',
-		'install_themes'
 	);
 	if (in_array($cap, $capabilities)) {
 		$caps[] = 'do_not_allow';
