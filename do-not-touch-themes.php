3680b4cfddcf1bb1ad677dbd66ddf0c70a9e8b74
GOUTEN <gouten@5010works.com>
2013-07-22 11:20:22 +0900

    要件が揃ったのでコードを整理

--- do-not-touch-themes.php
+++ do-not-touch-themes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Don't Touch Themes
 Plugin URI: https://github.com/gouten5010/do-not-touch-themes
-Description: We do not want to make a theme chosen, and do not want to let edit of WordPress plug-in and WordPress theme.
+Description: We do not want to make a theme chosen, and do not want to let edit of WordPress plug-in and WordPress theme. After all the setup and edits finish, please validate this plug-in.
 Version:0.1.1
 Author:GOUTEN
 Author URI: http://5010works.com/
@@ -10,35 +10,19 @@ License: GPL v2
 License URI:
 */
 
+//プラグインとテーマのインストール・更新を無効化
 function non_edit_theme_and_plugin() {
-	/*
-	テーマ・プラグインの編集をさせないようにする
-	(DISALLOW_FILE_MODSを使用する場合は削除するコト)
-	*/
-	if ( !defined ( 'DISALLOW_FILE_EDIT' ) ) {
-		define( 'DISALLOW_FILE_EDIT', true );
+
+	if ( !defined ( 'DISALLOW_FILE_MODS' ) ) {
+		define('DISALLOW_FILE_MODS',true);
 	}
-	/*
-	テーマ・プラグインの新規インストールとアップデート、編集をさせないようにする
-	*/
-	//if ( !defined ( 'DISALLOW_FILE_MODS' ) ) {
-	//	define('DISALLOW_FILE_MODS',true);
-	//}
 }
 add_action('plugins_loaded', 'non_edit_theme_and_plugin');
 
-function remove_theme_select_menus () {
-	/*
-	「テーマ」メニューを管理画面から除去
-	*/
-	remove_submenu_page( 'themes.php', 'themes.php' );
-}
-add_action( 'admin_menu', 'remove_theme_select_menus' );
-
+//テーマ選択の無効化
 function remove_role_options( $caps, $cap ) {
 	$capabilities = array(
-		'switch_themes',
-		'install_themes' //DISALLOW_FILE_MODSを使用する場合は削除してもいい
+		'switch_themes'
 	);
 	if (in_array($cap, $capabilities)) {
 		$caps[] = 'do_not_allow';
